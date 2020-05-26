822feb37d75df334e2b5361d03ea5d70c2713be8
vader2611 <57695639+vader2611@users.noreply.github.com>
2020-05-26 20:05:01 +0700

    1

--- app/src/main/java/vn/edu/poly/apptruyen/interfaces/LayChapVe.java
+++
@@ -1,7 +0,0 @@
-package vn.edu.poly.apptruyen.interfaces;
-
-public interface LayChapVe {
-    void batDau();
-    void ketThuc(String data);
-    void biLoi();
-}
--- app/src/main/java/vn/edu/poly/apptruyen/interfaces/LayTruyenVe.java
+++
@@ -1,7 +0,0 @@
-package vn.edu.poly.apptruyen.interfaces;
-
-public interface LayTruyenVe {
-    void batDau();
-    void ketThuc(String data);
-    void biLoi();
-}
